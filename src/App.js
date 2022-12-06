e6c494e2c8436dc5f4446153d07b819945d10f98
kachi33 <72194813+kachi33@users.noreply.github.com>
2022-12-06 02:52:38 +0100

    Navbar component export and import from Navbar.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './index.css';
+import Navbar from './Navbar';
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar/>
       <div className='content'>
         <h1> App Components</h1>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 const Navbar = () => {
+
     return ( 
         <nav className="navbar">
             <h1>Kachi's portfolio site</h1>
             <div className="links">
-                <a href={}>Home</a>
-                <a href={}>About</a>
-                <a href={}>Ho</a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Project</a>
             </div>
         </nav>
      );
